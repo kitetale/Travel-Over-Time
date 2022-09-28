c2fdcd77f5a59dadbcc80fb12844089d330f4968
Ashley Kim <kitetale248@gmail.com>
2022-09-28 01:07:13 -0400

    data per category

---
+++ create_data.py
@@ -0,0 +1,11 @@
+import os
+import cv2 as cv
+import numpy as numpy
+import time
+import uuid
+
+labels = ['street','slipon','athletic','design','hightop','lowtop']
+number_imgs = 10 #start with at least 10-20
+
+imgPath = '../data/sneakers'
+
