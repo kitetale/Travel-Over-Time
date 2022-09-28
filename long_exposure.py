c7bcd89a787134cc03d2b72aa2f61eb6915c106d
Ashley Kim <kitetale248@gmail.com>
2022-09-28 16:39:46 -0400

    stairs long exposure generated

--- long_exposure.py
+++ long_exposure.py
@@ -6,7 +6,8 @@ rAvg, gAvg, bAvg = None, None, None
 
 total = 0
 
-vid = cv2.VideoCapture("crop.mp4")
+#vid = cv2.VideoCapture("crop.mp4")
+vid = cv2.VideoCapture("stairs.mp4")
 
 while True:
     ret, frame = vid.read()
@@ -26,8 +27,12 @@ while True:
         gAvg = cv2.accumulateWeighted(G,gAvg,1/total)
         bAvg = cv2.accumulateWeighted(B,bAvg,1/total)
 
+    if (total % 1000 == 0) :
+        print(f"{total} images finished.")
+
 avg = cv2.merge([bAvg,gAvg,rAvg]).astype("uint8")
 
-cv2.imwrite("sidewalk_long_exposure.png",avg)
+# cv2.imwrite("sidewalk_long_exposure.png",avg)
+cv2.imwrite("stairs_long_exposure.png",avg)
 
 vid.release()
